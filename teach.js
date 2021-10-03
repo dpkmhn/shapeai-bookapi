57963a9f3d48cbd120b2258d8a8a61b140fe7233
dpkmhn <deepakmhn1@gmail.com>
2021-10-03 15:47:01 +0100

    initial

---
+++ teach.js
@@ -0,0 +1,21 @@
+
+//MongoDB operators 
+
+//Update Operators
+
+/*
+$inc -> increment
+$min -> minimum
+$max -> maximum
+$set -> sets a data to an object property
+$unset -> removes a property from an object
+
+
+
+Arrays
+$push
+$pop -> remove an element from end
+$pull -> remove element from middle
+$addToSet -> clever way to put data into an array
+
+*/
